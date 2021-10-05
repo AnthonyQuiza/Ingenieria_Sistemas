2dd20a89a401e69066c1a7685dcd24815c9ccb7e
Smuck <1620632@utp.edu.pe>
2021-10-05 00:29:59 -0500

    Ingenieria de Sistemas

---
+++ Main/AlertaCur.js
@@ -0,0 +1,29 @@
+Swal.fire ({
+    title: 'Cursos!',
+    //text: 'A mi pagina web',
+    //html: '<b class="rojo">Hola!</b>',
+    //footer: '<span class="rojo">Esta es la bienvenida!</span>',
+    width: '30%',
+    padding: '1rem',
+    //background: '',
+    backdrop: true,
+    //timer: 5000,
+    //timerProgressBar: true,
+    position: 'center',
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    allowEnterKey: false,
+    stopKeydownPropagation: false,
+    customClass: {
+        popup: 'popup-class'
+    },
+    showConfirmButtonColor: true,
+    showConfirmButtonColor: '#3E60E9',
+
+    showCloseButton: true,
+    closeButtonAriaLabel: 'Cerrar Alerta',
+
+    imageUrl: 'images/Cursos.png',
+    imageWidth: '200px',
+    imageAlt: 'Imagen de cursos',
+})
---
+++ Main/sweetAlert.js
@@ -0,0 +1,29 @@
+Swal.fire ({
+    title: 'Bienvenido!',
+    text: 'A mi pagina web',
+    //html: '<b class="rojo">Hola!</b>',
+    footer: '<span class="rojo">Esta es la bienvenida!</span>',
+    width: '30%',
+    padding: '1rem',
+    //background: '',
+    backdrop: true,
+    //timer: 5000,
+    //timerProgressBar: true,
+    position: 'center',
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    allowEnterKey: false,
+    stopKeydownPropagation: false,
+    customClass: {
+        popup: 'popup-class'
+    },
+    showConfirmButtonColor: true,
+    showConfirmButtonColor: '#3E60E9',
+
+    showCloseButton: true,
+    closeButtonAriaLabel: 'Cerrar Alerta',
+
+    imageUrl: 'images/bienvenido.jpg',
+    imageWidth: '200px',
+    imageAlt: 'Imagen de bienvenida',
+})
